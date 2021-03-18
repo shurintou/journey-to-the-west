f442a60e6ddfe089975c8ac81efe0254eaa0ed7a
shurintou <shurintou@gmail.com>
2021-03-18 19:49:59 +0900

    将chatRoom.vue的对话框逻辑解耦，新建chatroom/dialog模块

---
+++ src/components/chatRoom/dialogs/CreateGameRoomDialogModule.vue
@@ -0,0 +1,69 @@
+<template>
+    <el-dialog title="创建房间" :visible.sync="createGameRoomDialogVisible" :width="dialogWidth" center :modal="false" :close-on-click-modal="false" :before-close="closeCreateGameRoomDialog">
+        <el-form  :model="gameRoomValidateForm" ref="gameRoomValidateForm">
+          <el-form-item label="房间名" prop="roomName" :rules="[{ required: true, message: '请输入房间名', trigger: 'blur' }]">
+              <el-input placeholder="请输入房间名" type="text" v-model="gameRoomValidateForm.roomName" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password" :rules="[{trigger: 'blur', validator: checkPassword}]">
+              <el-input placeholder="非必填，4到8位数字" v-model="gameRoomValidateForm.password" autocomplete="off" maxlength="8" show-word-limit></el-input>
+          </el-form-item>
+        </el-form>
+         <div>
+          <span>使用牌数：{{ gameRoomValidateForm.cardNum }}副</span>
+          <el-slider v-model="gameRoomValidateForm.cardNum" :min="4" :max="10" :step="1" :show-tooltip="false"></el-slider>
+        </div>
+        <div slot="footer">
+            <el-button @click="closeCreateGameRoomDialog" style="margin-right:10%">取消</el-button>
+            <el-button type="success" @click="createGameRoom">创建</el-button>
+        </div>
+  </el-dialog>  
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            gameRoomValidateForm: {
+                roomName: this.$store.state.nickname + ' 的房间',
+                password: '', 
+                cardNum: 4,
+            },
+            checkPassword:  (rule, value, callback) => {
+                if (value === '') {
+                    callback();
+                }
+                else {
+                    var uPattern = /^[0-9]{4,8}$/
+                    if(!uPattern.test(value)){
+                    callback(new Error('密码须4到8位数字'));
+                    }
+                    else{
+                    callback();
+                    }
+                    callback();
+                }
+            },
+        }
+    },
+
+    props:{
+        createGameRoomDialogVisible: { type: Boolean, default: false },
+        dialogWidth: { type: String, default: '' },
+    },
+
+    methods:{
+        createGameRoom: function(){
+            console.log('create room')
+        },
+
+        closeCreateGameRoomDialog: function(){
+            this.$emit('createGameRoomDialogVisible', false)
+            this.gameRoomValidateForm.roomName = this.$store.state.nickname + ' 的房间'
+            this.gameRoomValidateForm.password = ''
+            this.gameRoomValidateForm.cardNum = 4
+            this.$refs.gameRoomValidateForm.clearValidate()
+        },
+    }
+}
+</script>
---
+++ src/components/chatRoom/dialogs/LogoutDialogModule.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-dialog title="提示" :visible.sync="cancelLeaveDialogVisible" :width="dialogWidth" center :before-close="closeLeaveDialog">
+        <i class="el-icon-question"></i>
+        <span style="text-align:center">确定要登出并离开游戏大厅吗？</span>
+        <span slot="footer">
+            <el-button @click="closeLeaveDialog" style="margin-right:10%">取消</el-button>
+            <el-button type="danger" @click="logOut">确定</el-button>
+        </span>
+  </el-dialog>  
+</template>
+
+<script>
+import { removeToken } from '../../../utils/cookie'
+import { logout } from '../../../api/login'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props:{
+        cancelLeaveDialogVisible: { type: Boolean, default: false },
+        dialogWidth: { type: String, default: '' },
+    },
+
+    methods:{
+        closeLeaveDialog: function(){
+            this.$emit('cancelLeaveDialogVisible', false)
+        },
+
+        logOut: function(){
+            this.$message({ message: '已登出，回到登录页面', type: 'info' });
+            logout().finally( () => {removeToken()});
+            this.$router.push({name: 'Login'})
+            this.closeLeaveDialog()
+        },
+    }
+}
+</script>
