2c5460bb7b4b7a0f2b35493d62480103641e53e8
shurintou <shurintou@gmail.com>
2021-03-26 01:31:17 +0900

    增加游戏房间空闲时桌面显示逻辑（纵向）

--- src/components/gameRoom/GameRoomTableModule.vue
+++ src/components/gameRoom/GameRoomTableModule.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="isItemHorizontal" id="game-room-table-horizontal-box">
+    <div v-if="isItemHorizontal" id="game-room-table-box">
         <div v-if="playerLocRoom.status === 0" id="game-room-table-horizontal-container">
             <div @click="function(){ if(playerLocRoom.owner === player.id) editGameRoomDialogVisible = true }">
                 <el-alert :style="{'font-size': fontSize}" :class="{'clickable': playerLocRoom.owner === player.id}" :title="playerLocRoom.name + ( playerLocRoom.owner === player.id ? ' [修改]' : '')" :description="  playerLocRoom.needPassword ? ' 密码： ' +  playerLocRoom.password : '' " type="info" center :closable="false"></el-alert>
@@ -13,13 +13,31 @@
                 <el-tag class="game-room-table-horizontal-record-item" type="danger" effect="dark" :size="tagSize" :style="{'font-size': fontSize}">{{ '拉跨： ' + player.loss + ' 局' }}</el-tag>
             </div>
         </div>
-        <div v-else>
+        <div v-else id="game-room-table-horizontal-container">
             <el-image :src="require('@/assets/images/poker/1A.png')"></el-image>
         </div>
         <EditGameRoomDialogModule :editGameRoomDialogVisible="editGameRoomDialogVisible" :playerLocRoom="playerLocRoom" :dialogWidth="dialogWidth" :ws="ws" @editGameRoomDialogVisible="function(value){ editGameRoomDialogVisible = value}"></EditGameRoomDialogModule>
     </div>
-    <div v-else>
-
+    <div v-else id="game-room-table-box">
+        <div v-if="playerLocRoom.status === 0" id="game-room-table-vertical-container">
+             <div @click="function(){ if(playerLocRoom.owner === player.id) editGameRoomDialogVisible = true }" id="game-room-table-vertical-room-info-top">
+                <el-alert :style="{'font-size': fontSize}" :class="{'clickable': playerLocRoom.owner === player.id}" :title="playerLocRoom.name + ( playerLocRoom.owner === player.id ? ' [修改]' : '')" :description="  playerLocRoom.needPassword ? ' 密码： ' +  playerLocRoom.password : '' " type="info" center :closable="false"></el-alert>
+            </div>
+            <div id="game-room-table-vertical-room-info-middle">
+                <el-tooltip  v-for="n in playerLocRoom.cardNum" :key="n" effect="light" :content="'游戏使用牌数： ' + playerLocRoom.cardNum + '副'" placement="right-start">
+                    <el-image :fit="'contain'" class="game-room-table-horizontal-poker-pool" :style="{'margin-left': n === 1 ? ( 50 - 5*playerLocRoom.cardNum ) + '' + '%': '0%' }" :src="require('@/assets/images/poker/poker-pool.png')"></el-image>
+                </el-tooltip>
+            </div>
+            <div id="game-room-table-vertical-room-info-bottom">
+                <el-tag class="game-room-table-horizontal-record-item" type="info" effect="dark" :size="tagSize" :style="{'font-size': fontSize}">{{ '吃牌数： ' + player.cards + ' 张' }}</el-tag>
+                <el-tag class="game-room-table-horizontal-record-item" type="success" effect="dark" :size="tagSize" :style="{'font-size': fontSize}">{{ '吃鸡： ' + player.win + ' 局' }}</el-tag>
+                <el-tag class="game-room-table-horizontal-record-item" type="danger" effect="dark" :size="tagSize" :style="{'font-size': fontSize}">{{ '拉跨： ' + player.loss + ' 局' }}</el-tag>
+            </div>
+        </div>
+        <div v-else id="game-room-table-vertical-container">
+            <el-image :src="require('@/assets/images/poker/1A.png')"></el-image>
+        </div>
+        <EditGameRoomDialogModule :editGameRoomDialogVisible="editGameRoomDialogVisible" :playerLocRoom="playerLocRoom" :dialogWidth="dialogWidth" :ws="ws" @editGameRoomDialogVisible="function(value){ editGameRoomDialogVisible = value}"></EditGameRoomDialogModule>
     </div>
 </template>
 
@@ -51,7 +69,7 @@ export default {
 
 <style>
 
-#game-room-table-horizontal-box{
+#game-room-table-box{
     width: 100%;
     height: 100%;
 }
@@ -75,6 +93,31 @@ export default {
 
 .game-room-table-horizontal-record-item{
     margin-left: 3%;
+}
+
+#game-room-table-vertical-container{
+    margin-left: 2%;
+    width: 100%;
+    height: 60vh;
+}
+
+#game-room-table-vertical-room-info-top{
+    width: 100%;
+    height: 10vh;
+    padding-top: 10vh;
+    text-align: center;
+}
+
+#game-room-table-vertical-room-info-middle{
+    width: 100%;
+    height: 10vh;
+    padding-top: 5vh;
+}
+
+#game-room-table-vertical-room-info-bottom{
+    margin-top: 15vh;
+    width: 100%;
+    height: 10vh;
 }
 
 .clickable:hover{
