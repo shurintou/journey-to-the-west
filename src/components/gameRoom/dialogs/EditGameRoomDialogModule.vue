02cbe5667ccb76385a4f3f39b28e60d7b746c403
shurintou <shurintou@gmail.com>
2021-03-25 17:50:16 +0900

    增加房主修改游戏房间设置的逻辑

--- src/components/gameRoom/dialogs/EditGameRoomDialogModule.vue
+++ src/components/gameRoom/dialogs/EditGameRoomDialogModule.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-dialog title="创建房间" :visible.sync="createGameRoomDialogVisible" :width="dialogWidth" center :modal="false" :close-on-click-modal="false" :before-close="closeCreateGameRoomDialog">
+    <el-dialog title="修改设置" :visible.sync="editGameRoomDialogVisible" :width="dialogWidth" center :modal="false" :close-on-click-modal="false" :before-close="closeEditGameRoomDialog">
         <el-form  :model="gameRoomValidateForm" ref="gameRoomValidateForm">
           <el-form-item label="房间名" prop="roomName" :rules="[{ required: true, message: '请输入房间名', trigger: 'blur' }]">
               <el-input placeholder="请输入房间名" type="text" v-model="gameRoomValidateForm.roomName" autocomplete="off"></el-input>
@@ -13,10 +13,10 @@
           <el-slider v-model="gameRoomValidateForm.cardNum" :min="4" :max="10" :step="1" :show-tooltip="false"></el-slider>
         </div>
         <div slot="footer">
-            <el-button @click="closeCreateGameRoomDialog" style="margin-right:10%">取消</el-button>
-            <el-button type="success" @click="createGameRoom">创建</el-button>
+            <el-button @click="closeEditGameRoomDialog" style="margin-right:10%">取消</el-button>
+            <el-button type="success" @click="editGameRoom">修改</el-button>
         </div>
-  </el-dialog>  
+    </el-dialog>  
 </template>
 
 
@@ -48,54 +48,50 @@ export default {
     },
 
     props:{
-        createGameRoomDialogVisible: { type: Boolean, default: false },
+        editGameRoomDialogVisible: { type: Boolean, default: false },
         dialogWidth: { type: String, default: '' },
         ws: { type: WebSocket, default: null},
+        playerLocRoom: { type: Object, default: null},
     },
 
     watch:{
-        createGameRoomDialogVisible: function(newVal){
+        editGameRoomDialogVisible: function(newVal){
             if(newVal === true){
-                this.gameRoomValidateForm.roomName = this.$store.state.nickname + ' 的房间'
+                this.gameRoomValidateForm.roomName = this.playerLocRoom.name
+                this.gameRoomValidateForm.password = this.playerLocRoom.needPassword ? this.playerLocRoom.password : ''
+                this.gameRoomValidateForm.cardNum = this.playerLocRoom.cardNum
             }
         }
     },
 
     methods:{
-        createGameRoom: function(){
+        editGameRoom: function(){
             this.$refs.gameRoomValidateForm.validate(valid => {
                 if(valid){
+                    if( this.gameRoomValidateForm.roomName === this.playerLocRoom.name && this.gameRoomValidateForm.password === this.playerLocRoom.password && this.gameRoomValidateForm.cardNum === this.playerLocRoom.cardNum ){
+                        this.closeEditGameRoomDialog()
+                        return
+                    }
                     this.ws.send(JSON.stringify({ 
                         type: 'gameRoomList',
-                        id: NaN, 
+                        action: 'edit',
+                        id: this.playerLocRoom.id, 
                         name: this.gameRoomValidateForm.roomName, 
-                        status: 0, 
                         needPassword: this.gameRoomValidateForm.password.length > 0 ? true: false,
                         password: this.gameRoomValidateForm.password, 
                         cardNum: this.gameRoomValidateForm.cardNum, 
-                        owner: this.$store.state.id, 
-                        playerList: { 
-                            0: {id: this.$store.state.id, cards: 0, win: 0, loss: 0, ready: false},
-                            1: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            2: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            3: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            4: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            5: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            6: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                            7: {id: 0, cards: 0, win: 0, loss: 0, ready: false},
-                        }
                     }))
-                    this.closeCreateGameRoomDialog()
+                    this.closeEditGameRoomDialog()
                 }
             })
         },
 
-        closeCreateGameRoomDialog: function(){
-            this.$emit('createGameRoomDialogVisible', false)
+        closeEditGameRoomDialog: function(){
+            this.$emit('editGameRoomDialogVisible', false)
             this.$refs.gameRoomValidateForm.clearValidate()
-            this.gameRoomValidateForm.roomName = this.$store.state.nickname + ' 的房间'
-            this.gameRoomValidateForm.password = ''
-            this.gameRoomValidateForm.cardNum = 4
+            this.gameRoomValidateForm.roomName = this.playerLocRoom.name
+            this.gameRoomValidateForm.password = this.playerLocRoom.needPassword ? this.playerLocRoom.password : ''
+            this.gameRoomValidateForm.cardNum = this.playerLocRoom.cardNum
         },
     }
 }
