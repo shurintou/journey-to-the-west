bd80430195490530cd40423ea1a6ee66f9986f27
shurintou <shurintou@gmail.com>
2021-03-31 23:33:48 +0900

    增加出牌逻辑

--- src/components/gameRoom/CardModule.vue
+++ src/components/gameRoom/CardModule.vue
@@ -1,8 +1,10 @@
 <template>
     <el-main class="hide-scroll-bar" :style="{backgroundImage: 'url(' + horizontalBackground + ')'}">
         <div id="card-module-top" v-if="getGamePlayer !== null">
-            <el-tooltip  v-for="(cardIndex, n) in getGamePlayer.remainCards" :key="n" effect="light" placement="top" :content="cardList[cardIndex].name">
-                <el-image fit="contain" style="width: 20%; height:80%" :style="{'margin-left': n === 0 ? ( 50 - 10*getGamePlayer.remainCards.length ) + '' + '%': '0%' }" :src="require('@/assets/images/poker/' + cardList[cardIndex].src  +'.png')"></el-image>
+            <el-tooltip v-for="(cardIndex, n) in sortCardList" :key="cardIndex + n" effect="light" placement="top" :content="cardList[cardIndex].name">
+                <div class="poker-card-item" :style="{'margin-left': n === 0 ? ( 50 - 7.5*getGamePlayer.remainCards.length ) + '' + '%': '0%' }">
+                    <el-image fit="fill" class="card-item" :class="{'is-card-selected' : selectCard.includes(n), 'card-not-selected' : !selectCard.includes(n)}" :src="require('@/assets/images/poker/' + cardList[cardIndex].src  +'.png')" @click="addSelectCard(n, cardIndex)"></el-image>
+                </div>
             </el-tooltip>
         </div>
         <div id="card-module-bottom" v-if="getGamePlayer !== null">
@@ -21,23 +23,25 @@ import { cardList } from '../../mixins/gameRoom/cardList'
 export default {
     data(){
         return {
-            // gameInfo: {
-            //     id: 0,
-            //     remainCards: 100,
-            //     clockwise: false,
-            //     currentPlayer: 0,
-            //     currentCard: [15],
-            //     currentCardPlayer: 0,
-            //     currentCombo: 0,
-            //     gamePlayer: {
-            //         0: {id: 10, nickname: '', cards: 0, remainCards: [50,49,35,51,52], maxCombo: 0, online: true, wukong: 0, bajie: 0, shaseng: 0, tangseng: 0, joker: 0},
-            //     }    
-            // },
+            selectCard:[],
+            gameInfo: {
+                id: 0,
+                remainCards: 100,
+                clockwise: false,
+                currentPlayer: 0,
+                currentCard: [15,15],
+                currentCardPlayer: 0,
+                currentCombo: 0,
+                gamePlayer: {
+                    0: {id: 10, nickname: '', cards: 0, remainCards: [52,49,35,51,52], maxCombo: 0, online: true, wukong: 0, bajie: 0, shaseng: 0, tangseng: 0, joker: 0},
+                    1: {id: 3, nickname: '', cards: 0, remainCards: [50,49,35,51,52], maxCombo: 0, online: true, wukong: 0, bajie: 0, shaseng: 0, tangseng: 0, joker: 0},
+                }    
+            },
         }
     },
 
     props:{
-        gameInfo: { type: Object, default: null },
+        // gameInfo: { type: Object, default: null },
         horizontalBackground: {type: String, default: ''},
         buttonSize: {type: String, default: ''},
         fontSize: {type: String, default: ''},
@@ -46,17 +50,65 @@ export default {
 
     computed:{
         getGamePlayer: function(){
+            if(!this.gameInfo) return null
             for( let i = 0; i < Object.keys(this.gameInfo.gamePlayer).length; i++ ){
                 if(this.gameInfo.gamePlayer[i].id === this.$store.state.id){
                     return this.gameInfo.gamePlayer[i]
                 }
             }
             return null
-        }
+        },
+
+        sortCardList: function(){
+            if(this.getGamePlayer === null) return null
+            let sortedList = this.getGamePlayer.remainCards
+            return sortedList.sort((a,b) =>{
+                return (this.cardList[a].num + this.cardList[a].suit) - (this.cardList[b].num + this.cardList[b].suit)
+            })
+        },
     },
 
     methods:{
-       
+        addSelectCard: function(n, cardIndex){
+            if(this.selectCard.length === 0){
+                this.selectCard.push(n)
+            }
+            else{
+                /* 已选择的牌则取消选择 */
+                if(this.selectCard.includes(n)){
+                    this.selectCard = this.selectCard.filter( card => card !== n)
+                    return
+                }
+                /* 判断是否超过长度 */
+                if(this.selectCard.length < this.gameInfo.currentCard.length){
+                    /* 未超过长度，判断牌是否同一类型，是同一类型加入数组 */
+                    if(this.cardList[this.sortCardList[this.selectCard[0]]].num === this.cardList[cardIndex].num){
+                        this.selectCard.push(n)
+                    }
+                    /* 不是则清空数组，重新加入新的牌型 */
+                    else{
+                        while(this.selectCard.length > 0){
+                            this.selectCard.pop()
+                        }
+                        this.selectCard.push(n)
+                    }
+                }
+                else{
+                    /* 超过长度，判断牌是否同一类型，是同一类型则弹出最后加入的牌 */
+                    if(this.cardList[this.sortCardList[this.selectCard[0]]].num === this.cardList[cardIndex].num){
+                        this.selectCard.shift()
+                        this.selectCard.push(n)
+                    }
+                    /* 不是则清空数组，重新加入新的牌型 */
+                    else{
+                        while(this.selectCard.length > 0){
+                            this.selectCard.pop()
+                        }
+                        this.selectCard.push(n)
+                    }
+                }
+            }
+        }
     },
 
     mixins:[ cardList ],
@@ -72,5 +124,36 @@ export default {
 #card-module-bottom{
     width: 100%;
     height: 20%;
+}
+
+.poker-card-item{
+    width: 13%;
+    height: 80%;
+    margin-right: 2%;
+    display: inline-block;
+}
+
+.is-card-selected{
+    border-width: 2%;
+    border-radius: 8px;
+    border-color: greenyellow;
+    border-style: solid;
+}
+
+.card-not-selected{
+    border-width: 2%;
+    border-radius: 8px;
+    border-color: rgba(121, 122, 119, 0);
+    border-style: solid;
+}
+
+.card-item{
+    width: 100%; 
+    height: 100%;
+    
+}
+
+.poker-card-item:hover{
+    cursor: pointer;
 }
 </style>
