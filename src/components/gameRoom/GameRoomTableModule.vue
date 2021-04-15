16c0554042c65ca2d47a6dbf752f4a8c379dc829
shurintou <shurintou@gamil.com>
2021-04-15 23:44:14 +0900

    增加出牌顺序旋转动画

--- src/components/gameRoom/GameRoomTableModule.vue
+++ src/components/gameRoom/GameRoomTableModule.vue
@@ -46,8 +46,8 @@
                 </div>
                 <div class="game-room-table-horizontal-box-middle-item">
                     <p class="white-color-font" :style="{'font-size': fontSize}">出牌顺序</p>
-                    <el-image v-if="gameInfo.clockwise" :src="require('@/assets/images/clockwise.png')" style="max-width:4vw"></el-image>
-                    <el-image v-else :src="require('@/assets/images/anti-clockwise.png')" style="max-width:4vw"></el-image>
+                    <el-image v-if="gameInfo.clockwise" :src="require('@/assets/images/clockwise.png')" style="max-width:4vw" class="clockwise-rotate"></el-image>
+                    <el-image v-else :src="require('@/assets/images/anti-clockwise.png')" style="max-width:4vw" class="anti-clockwise-rotate"></el-image>
                     <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.clockwise ? '顺时针' : '逆时针'}}</div>
                 </div>
             </div>
@@ -130,8 +130,8 @@
                 </div>
                 <div class="game-room-table-vertical-box-bottom-item">
                     <div class="white-color-font" style="min-width: 40px" :style="{'font-size':fontSize}">出牌顺序</div>
-                    <el-image v-if="gameInfo.clockwise" :src="require('@/assets/images/clockwise.png')" style="max-width:4vw"></el-image>
-                    <el-image v-else :src="require('@/assets/images/anti-clockwise.png')" style="max-width:4vw"></el-image>
+                    <el-image v-if="gameInfo.clockwise" :src="require('@/assets/images/clockwise.png')" style="max-width:4vw" class="clockwise-rotate"></el-image>
+                    <el-image v-else :src="require('@/assets/images/anti-clockwise.png')" style="max-width:4vw" class="anti-clockwise-rotate"></el-image>
                     <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.clockwise ? '顺时针' : '逆时针'}}</div>
                 </div>
             </div>
@@ -459,4 +459,37 @@ export default {
     margin-left: 20%;
 }
 
+.clockwise-rotate{
+    -webkit-animation: clockwiseRotate 10s linear infinite;
+    animation: clockwiseRotate 10s linear infinite;
+}
+
+.anti-clockwise-rotate{
+    -webkit-animation: antiClockwiseRotate 10s linear infinite;
+    animation: antiClockwiseRotate 10s linear infinite;
+}
+
+@-webkit-keyframes clockwiseRotate{
+    0%{ -webkit-transform: rotate(0deg);}
+    50%{ -webkit-transform: rotate(180deg);}
+    100%{ -webkit-transform: rotate(360deg);}
+}
+
+@keyframes clockwiseRotate{
+    0%{ -webkit-transform: rotate(0deg);}
+    50%{ -webkit-transform: rotate(180deg);}
+    100%{ -webkit-transform: rotate(360deg);}
+}
+
+@-webkit-keyframes antiClockwiseRotate{
+    0%{ -webkit-transform: rotate(0deg);}
+    50%{ -webkit-transform: rotate(-180deg);}
+    100%{ -webkit-transform: rotate(-360deg);}
+}
+
+@keyframes antiClockwiseRotate{
+    0%{ -webkit-transform: rotate(0deg);}
+    50%{ -webkit-transform: rotate(-180deg);}
+    100%{ -webkit-transform: rotate(-360deg);}
+}
 </style>
