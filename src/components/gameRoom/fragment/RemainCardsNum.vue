2c6bd1759e9fe5221e0afa8638742de5ed8ee0ee
shurintou <shurintou@gamil.com>
2021-04-19 15:14:18 +0900

    将游戏桌面显示的剩余牌数，连击牌数，出牌顺序解耦

---
+++ src/components/gameRoom/fragment/RemainCardsNum.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="isHorizontal" style="margin-left: 10%;" class="game-room-table-horizontal-box-middle-item">
+        <p class="white-color-font" :style="{'font-size': fontSize}">剩余牌数</p>
+        <el-image :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
+        <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.remainCards}}张</div>
+    </div>
+    <div v-else class="game-room-table-vertical-box-bottom-item">
+        <div class="white-color-font" :style="{'font-size':fontSize}">剩余牌数</div>
+        <el-image :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
+        <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.remainCards}}张</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+
+        }
+    },
+
+    props:{
+        gameInfo: { type: Object, default: null },
+        fontSize: { type: String, default: ''},
+        isHorizontal: { type: Boolean, default: false},
+    },
+}
+</script>
