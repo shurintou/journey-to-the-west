300eaab5f4794395b35c41ed1af49b2b4dce6929
shurintou <shurintou@gmail.com>
2021-04-19 16:57:41 +0900

    增加剩余牌数的动画

--- src/components/gameRoom/fragment/RemainCardsNum.vue
+++ src/components/gameRoom/fragment/RemainCardsNum.vue
@@ -1,13 +1,21 @@
 <template>
     <div v-if="isHorizontal" style="margin-left: 10%;" class="game-room-table-horizontal-box-middle-item">
-        <p class="white-color-font" :style="{'font-size': fontSize}">剩余牌数</p>
-        <el-image :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
-        <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.remainCards}}张</div>
+        <transition leave-active-class="scale-out-top">
+            <p v-show="gameInfo.remainCards > 0" class="white-color-font" :style="{'font-size': fontSize}">剩余牌数</p>
+        </transition>
+        <transition leave-active-class="scale-out-top">
+            <el-image v-show="gameInfo.remainCards > 0" :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
+        </transition>
+        <div class="white-color-font" :style="{'font-size':fontSize, 'color': gameInfo.remainCards > 0 && gameInfo.remainCards < 10 ? 'red' : 'white'}">{{gameInfo.remainCards > 0 ? gameInfo.remainCards + ' 张' : '已发完'}}</div>
     </div>
     <div v-else class="game-room-table-vertical-box-bottom-item">
-        <div class="white-color-font" :style="{'font-size':fontSize}">剩余牌数</div>
-        <el-image :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
-        <div class="white-color-font" :style="{'font-size':fontSize}">{{gameInfo.remainCards}}张</div>
+        <transition leave-active-class="scale-out-top">
+            <div v-show="gameInfo.remainCards > 0" class="white-color-font" :style="{'font-size':fontSize}">剩余牌数</div>
+        </transition>
+        <transition leave-active-class="scale-out-top">
+            <el-image v-show="gameInfo.remainCards > 0" :src="require('@/assets/images/poker/poker-pool.png')" style="max-width:3vw"></el-image>
+        </transition>
+        <div class="white-color-font" :style="{'font-size':fontSize, 'color': gameInfo.remainCards > 0 && gameInfo.remainCards < 10 ? 'red' : 'white'}">{{gameInfo.remainCards > 0 ? gameInfo.remainCards + ' 张' : '已发完'}}</div>
     </div>
 </template>
 
@@ -26,3 +34,8 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.scale-out-top{-webkit-animation:scale-out-top .5s cubic-bezier(.55,.085,.68,.53) both;animation:scale-out-top .5s cubic-bezier(.55,.085,.68,.53) both}
+@-webkit-keyframes scale-out-top{0%{-webkit-transform:scale(1);transform:scale(1);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}100%{-webkit-transform:scale(0);transform:scale(0);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}}@keyframes scale-out-top{0%{-webkit-transform:scale(1);transform:scale(1);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}100%{-webkit-transform:scale(0);transform:scale(0);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}}
+</style>
