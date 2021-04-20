caae6f7ce5c972b83770987fcdfd28860d0026a5
shurintou <shurintou@gmail.com>
2021-04-20 21:12:33 +0900

    增加玩家自身收牌时游戏桌面上有关玩家自身img的动画逻辑

--- src/components/gameRoom/fragment/RemainCardsNum.vue
+++ src/components/gameRoom/fragment/RemainCardsNum.vue
@@ -35,7 +35,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.scale-out-top{-webkit-animation:scale-out-top .5s cubic-bezier(.55,.085,.68,.53) both;animation:scale-out-top .5s cubic-bezier(.55,.085,.68,.53) both}
-@-webkit-keyframes scale-out-top{0%{-webkit-transform:scale(1);transform:scale(1);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}100%{-webkit-transform:scale(0);transform:scale(0);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}}@keyframes scale-out-top{0%{-webkit-transform:scale(1);transform:scale(1);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}100%{-webkit-transform:scale(0);transform:scale(0);-webkit-transform-origin:50% 0;transform-origin:50% 0;opacity:1}}
-</style>
