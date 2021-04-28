ce11d4f7dd524e6d15b9324e681d9d5558f27949
shurintou <shurintou@gamil.com>
2021-04-28 17:19:37 +0900

    新增查询显示排行榜逻辑

---
+++ src/api/getRank.js
@@ -0,0 +1,9 @@
+import request from '../utils/request'
+
+export function getRankInfo(params){
+    return request({
+        url: '/rank/',
+        params: { type : params.type, id : params.id },
+        method: 'get',
+    })
+}
