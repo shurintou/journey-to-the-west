8e212747b01ae0ec0d37e64a609e451773044994
shurintou <shurintou@gmail.com>
2023-09-24 01:49:53 +0900

    refactor: 执行 'vue add typescript' 命令.

---
+++ src/type/index.ts
@@ -0,0 +1,10 @@
+export interface GameResultsList {
+    id: number,
+    nickname: string,
+    cards: number,
+    avatar_id: number,
+    seat_index: number,
+    createdAt: string,
+    gameId: number,
+    max_combo: number,
+}
