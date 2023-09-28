2698f644f146fe40afce56a9445cd8bf26c4ffb4
shurintou <shurintou@gmail.com>
2023-09-29 02:26:57 +0900

    chore: 补充参数和其类型。

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 import { ResponseDataCommon } from '@/type/request'
 
-export function register(data: { username: string, password: string }): Promise<ResponseDataCommon> {
+export function register(data: { username: string, password: string, invitationCode: string }): Promise<ResponseDataCommon> {
     return request({
         url: '/register',
         method: 'post',
